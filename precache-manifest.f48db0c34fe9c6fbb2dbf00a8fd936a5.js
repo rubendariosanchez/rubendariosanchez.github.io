558630845b2948b9e7186f2c13e0b2386bd76518
Rubén Sánchez <sanchezramirez.rubendario>
2020-08-06 09:17:59 -0500

    Updates

---
+++ precache-manifest.f48db0c34fe9c6fbb2dbf00a8fd936a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091208ad482e2527b7f90f543ac8e829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45bfbf854df9b741781a",
+    "url": "/static/css/main.047e85cd.chunk.css"
+  },
+  {
+    "revision": "636216db22470e44d936",
+    "url": "/static/js/2.e2c2b74f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e2c2b74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bfbf854df9b741781a",
+    "url": "/static/js/main.f1846360.chunk.js"
+  },
+  {
+    "revision": "30516ebdb57b4b986610",
+    "url": "/static/js/runtime-main.fac1c025.js"
+  }
+]);
